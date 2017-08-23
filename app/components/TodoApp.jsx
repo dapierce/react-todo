91558e02e52a6676daaa76e202fce4b151917b3e
dapierce <pierceda@gmail.com>
2017-08-23 15:55:11 -0700

    Displays static todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,35 @@
 var React = require('react');
 
+var TodoList = require('TodoList');
+
 class TodoApp extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          text: 'Feed the cat'
+        }, {
+          id: 2,
+          text: 'Clean the kitchen'
+        }, {
+          id: 3,
+          text: 'Cook something!'
+        }, {
+          id: 4,
+          text: 'Make React Todo app'
+        }
+      ]
+    }
+  }
   render () {
+    var {todos} = this.state;
+
     return (
       <div>
-        TodoApp
+        <h1>Todo</h1>
+        <TodoList todos={todos}/>
       </div>
     )
   }
