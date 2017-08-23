568f44fee1a6a5e08a418d8fc024f329aa6d6384
dapierce <pierceda@gmail.com>
2017-08-23 15:57:37 -0700

    Displays static todo list

---
+++ app/components/TodoList.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+
+var TodoItem = require('TodoItem');
+
+class TodoList extends React.Component {
+  render () {
+    var {todos} = this.props;
+
+    // use map to iterate each element, using key for React elements
+    var renderTodos = () => {
+      return todos.map((todo) => {
+        return (
+          <TodoItem key={todo.id} {...todo}/>
+        )
+      });
+    };
+    
+    return (
+      <div>
+        {renderTodos()}
+      </div>
+    )
+  }
+}
+
+module.exports = TodoList;
